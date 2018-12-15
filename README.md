feae763d2b43a5e7141a2295d52dea31eab0e0c3
Marc Neudert <marc.neudert@gmail.com>
2018-12-15 11:19:19 +0100

    bootstraps nix-shell

---
+++ README.md
@@ -0,0 +1,1 @@
+Elixir Inspector Nix-Shell
---
+++ default.nix
@@ -0,0 +1,24 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+  name = "nix-shell-elixir-inspector";
+  env  = buildEnv {
+    name  = name;
+    paths = buildInputs;
+  };
+
+  shellHook = ''
+    export SHELL_DATA_DIR="$(pwd)/runtime"
+
+    export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_path '\"$SHELL_DATA_DIR/erlang-history\"'"
+    export HEX_HOME="$SHELL_DATA_DIR/hex"
+    export MIX_HOME="$SHELL_DATA_DIR/mix"
+    export PS1="[elixir-inspector|\[\e[1m\]\w\[\e[0m\]]$ "
+  '';
+
+  elixir = pkgs.callPackage ./packages/elixir {};
+
+  buildInputs = [
+    elixir
+  ];
+}
